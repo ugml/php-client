e8477e7531bcc14c5a61b933ed2d5e12c872f71f
mamen <markusme@edu.aau.at>
2018-02-08 14:46:50 +0100

    testing travis ci n+1

--- tests/units/helloWorld.php
+++ tests/units/helloWorld.php
@@ -4,17 +4,43 @@
 
 //    require_once 'vendor/bin/atoum.phar';
 
-    include_once 'core/classes/helloWorld.php';
+    include_once 'core/classes/user.class.php';
 
     use mageekguy\atoum;
     use core\classes;
 
-    class helloWorld extends atoum\test
+    class userTests extends atoum\test
     {
-        public function testSay()
-        {
-            $helloWorld = "Hello World!";
 
-            $this->string($helloWorld)->isEqualTo("Hello World!");
+        public function testGetUserID()
+        {
+            $user = new User(123, "testname", "email@mail.at", time(), 0);
+            $this->integer($user->getUserID())->isEqualTo(123);
+
         }
+
+        public function testGetUsername() {
+            $user = new User(123, "testname", "email@mail.at", time(), 0);
+            $this->string($user->getUsername())->isEqualTo("testname");
+        }
+
+        public function testGetEmail() {
+            $user = new User(123, "testname", "email@mail.at", time(), 0);
+            $this->string($user->getEmail())->isEqualTo("email@mail.at");
+        }
+
+        public function testGetOnlineTime() {
+
+            $ot = time();
+
+            $user = new User(123, "testname", "email@mail.at", $ot, 0);
+            $this->string($user->getOnlineTime())->isEqualTo($ot);
+        }
+
+        public function testGetCurrentPlanet() {
+            $user = new User(123, "testname", "email@mail.at", time(), 0);
+            $this->string($user->getCurrentPlanet())->isEqualTo(0);
+        }
+
+
     }
