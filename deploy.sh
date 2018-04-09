0ab053e751235507f2d8ddf746cc2d26aa01dfc9
mamen <markusme@edu.aau.at>
2018-04-09 15:52:10 +0200

    - travis-update

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# create documentation
-php ./vendor/bin/phpdoc -i "css/,images/,scripts/,skins/,tests/,vendor/,core/config.php,test.php" -d ./ --template clean --title "ugamela Documentation"
-mv output/ docs/
-rm -rf ./docs/build/
-
-curl --ftp-create-dirs -T ./ -u $SFTP_USER:$SFTP_PASSWORD ftp://$SFTP_HOST/ugamela/$f
